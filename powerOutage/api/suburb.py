3f845d060a62e87180793f4bf318a43f36368d6b
Callum <callum@bbd.co.za>
2022-06-09 10:59:59 +0200

    chore: intial commit

---
+++ powerOutage/api/suburb.py
@@ -0,0 +1,3 @@
+class suburb:
+    def testing(self):
+        print("Gotta get the info here")
---
+++ powerOutage/powerOutage.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from api.suburb import suburb
+"""
+Module PowerOutage
+"""
+
+__author__ = "Callum Dunne"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def main():
+    test = suburb()
+    test.testing()
+    print("hello world")
+
+
+if __name__ == "__main__":
+    main()
